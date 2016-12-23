1edb82b62d8e4ef39d4039e3db1cf9483e2df162
D <MacMini@Abhijits-Mac-mini.local>
2016-12-22 22:37:17 -0800

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt){    
+    require('load-grunt-tasks')(grunt);
+
+    var config = grunt.file.readYAML('gruntConfig.yml');
+
+    grunt.initConfig({
+
+        copy:{
+            main: {
+                files: [
+                {expand: true, src: ['src/**'], dest: 'dist/'}
+                ]
+            }
+        },
+
+        // sass:{
+        //     dist:{
+        //         src: config.jsSrcDir + 'style.js',
+        //         dest: config.cssDir + 'style.css'
+        //     }
+        // },
+
+        // concat:{
+        //     dist:{
+        //         src: config.jsSrcDir +'*.js',
+        //         dest: config.jsConcatDir + 'main.js'
+        //     }
+        // },
+
+        jshint:{
+            all:[
+            'gruntfile.js',
+            config.jsSrcDir + '*.js'
+            ]
+        }
+
+        // watch:{
+        //     sass:{
+        //         files: config.scssDir + '**/*.scss',
+        //         tasks: ['sass']
+        //     }
+        // }
+    });
+
+    grunt.registerTask('default',[
+        'jshint',
+        'copy'
+        ]);
+};
